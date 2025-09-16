2cde6d6656ff8196159c7f84affc3da2efa22363
hammadtech16 <hammadriaz170@gmail.com>
2025-09-16 14:36:32 -0700

    Header section commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,16 @@ function App() {
         </ul>
         <button class='w-32  bg-amber-300 rounded-2xl hover:bg-amber-400 font-bold '>Sign Up</button>
       </nav>
-      
+      <header class=' pt-8 flex justify-between mt-10'>
+        <div class='flex flex-col items-start gap-6'>
+          <p class='uppercase text-black'>Try it Now!</p>
+          <h2 class='text-5xl text-black m-4 font-medium w-100 text-left '>Change the way you use your <span class='text-sky-500 font-serif text-6xl m-4'>Money</span></h2>
+          <button class='w-42 h-12   bg-amber-300 rounded-3xl hover:bg-amber-400 font-bold '>Get Started Now</button>
+        </div>
+        <div class=' w-1xl '>
+          <img src="\src\assets\solar-img.jpg" class='rounded-2xl'  alt="" srcset="" />
+        </div>
+      </header>
     </>
   )
 }
